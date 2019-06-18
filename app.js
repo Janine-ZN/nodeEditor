64f1d262112ea44f5cebad0daca76ac79ff9a752
Janine-ZN <Janine_z@163.com>
2019-06-18 17:56:21 +0800

    1.解决req.body为空 2.进一步完善list和add请求

--- app.js
+++ app.js
@@ -4,15 +4,30 @@ const editorRouter = require('./routes/editor.js');
 
 //创建web服务器
 var server = express();
+
+//allow custom header and CORS
+server.all('*', function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+
+    if (req.method == 'OPTIONS') {
+        res.send(200); // 让options请求快速返回
+    } else {
+        next();
+    }
+});
+
 server.listen(3000);
 
-//托管静态资源到public目录下
-server.use(express.static('public'));
 //使用body-parser中间件将post请求数据解析为对象
 //注意：一定要写在路由的前边
 server.use(bodyParser.urlencoded({
-    extended: false
+    extended: true
 }));
+
+server.use(bodyParser.json());
+
 
 //把 编辑 路由器挂载到/editor
 // /editor/list
--- routes/editor.js
+++ routes/editor.js
@@ -14,23 +14,34 @@ router.get('/list', (req, res) => {
     });
 });
 
-router.get('/add', (req, res) => {
-    var content = `router.get('/list', (req, res) => {
-            pool.query('SELECT * FROM news', [], (err, result) => {
-                if (err) throw err;
-                res.send(result);
-            });
-        });`;
-    pool.query(`insert into news values(?,?,?,?,?,?)`, [null, "agent说明", content, '2019-02-03', '2019-06-06', 1], (err, result) => {
-        if (err) throw err;
-        //是否添加成功
-        if (result.affectedRows > 0) {
-            res.send({
-                code: 200,
-                msg: 'register suc'
-            });
-        }
-    });
+router.post('/add', (req, res) => {
+    var obj = req.body;
+    console.log("object对象==》" +JSON.stringify(obj));
+    var $menu = obj.menu;
+    var $submenu = obj.submenu
+    var $content = obj.content;
+    console.log("内容==》" + $content, "一级菜单==》" + $menu, "二级菜单==》" + $submenu);
+
+    pool.query("INSERT INTO news (id,menu,submenu,content,created_time,last_modified_time) SELECT ?,?,?,?,?,? from DUAL where not exists(select content from news where content = ?)",
+        [null, $menu, $submenu, $content, '2019-02-03', '2019-06-06', $content], (err, result) => {
+            if (err) throw err;
+            console.log(111);
+            console.log(result);
+            if (result.affectedRows > 0) {
+                res.send({
+                    code: 200,
+                    msg: '数据插入成功！'
+                });
+            }
+
+            //是否添加成功
+            if (result.affectedRows == 0) {
+                res.send({
+                    code: 200,
+                    msg: '数据标题重复！'
+                });
+            }
+        });
 });
 
 //导出路由器
